c67bd2fcebb69a2c1e69a8db91c100cc42cf93a4
DmitaN <66790897+DmitaN@users.noreply.github.com>
2020-06-15 23:31:40 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 def app(environ, start_response):
     q_string = environ.get('QUERY_STRING')
-    print("!!!!!")
-    print(q_string)
-    print("!!!!!")
-    return q_string
+    pairs_list = q_string.split("&")
+    body = ""
+    for pair in pairs_list:
+        body += pair + "\n"
+    response_string = [('Content-type', 'text/plain')]
+    start_response('200 OK', response_string)
+    return [body.strip()]
