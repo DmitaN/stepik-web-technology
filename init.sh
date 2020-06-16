b156532e44342cb04ad9eba733af5810d68d87d7
Alex <dmitan@bk.ru>
2020-06-16 11:40:12 +0300

    url

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:app 
+sudo gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:app 
+sudo gunicorn -b 0.0.0.0:8000 -c ~/web/ask ask.wsgi:application 
