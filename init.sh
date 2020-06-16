273cb410fab093555010eb60b04025af04378ded
Alex <dmitan@bk.ru>
2020-06-16 13:01:26 +0300

    ask3

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
