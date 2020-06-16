274cb5f85c700bdf375678b90c1eacaeb113f057
Alex <dmitan@bk.ru>
2020-06-16 12:16:08 +0300

    settings

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8000 -c ~/web/ask/ask ask.wsgi:application 
+cd ~/web/ask
+sudo gunicorn -b 0.0.0.0:8000 -c  ask.wsgi:application 
