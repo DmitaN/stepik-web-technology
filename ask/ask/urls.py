b3d5bc3a294cb5d1404d8c4d88f8279b36bd79ab
DmitaN <66790897+DmitaN@users.noreply.github.com>
2020-06-16 13:21:37 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import include, url, patterns 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^$', include('qa.urls')),
     url(r'^login/', include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
