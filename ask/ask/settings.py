f2ca4f07600dd71426c787cfda0564b313f00a07
DmitaN <66790897+DmitaN@users.noreply.github.com>
2020-06-16 13:20:01 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9g^e48696c6$qiet#1l##9j-#ksba%hxu$f$jd7v8#)l#=d$ah'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
